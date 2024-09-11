d36817676d55323082f2c573fec380c5cfa99c16
LILLY MOORE <LAM22Q@FSU.EDU>
2024-09-10 20:23:09 -0400

    2nd commit part 2

--- sketch.js
+++ sketch.js
@@ -3,26 +3,49 @@ let y1 = 0;       // y position of the ball
 let x1Speed = 3;  // speed in X direction
 let y1Speed = 7; // speed in Y direction
 let d1 = 20;      // diameter of the ball
-
+//second shape
+let x = 50;     // x position of the ball
+let y2 = 90;       // y position of the ball
+let x2Speed = 3;  // speed in X direction
+let y2Speed = 7; // speed in Y direction
+let d2 = 20;      // diameter of the ball
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220); // try commenting this out!
- 
+
   // if x hits the right side or the left side of the canvas,
   // switch directions...
-  if (x1 > width || x < 0) {
-    x1Speed = x1Speed * -1;  
+  if (x1 > width || x1 < 0) {
+    x1Speed = x1Speed * -1;
   }
   x1 = x1 + x1Speed; // adding a negative xSpeed *decreases* x, right?
-  
+
   // same as above, but for the top and bottom of the canvas
   if (y1 > width || y1 < 0) {
-    y1Speed = y1Speed * -1;  
+    y1Speed = y1Speed * -1;
   }
   y1 = y1 + y1Speed;
 
-  ellipse(x1, y1, d1);
+  circle(x1, y1, d1);
+
+  //second shape
+
+  // if x hits the right side or the left side of the canvas,
+  // switch directions...
+  if (x2 > width || x2 < 0) {
+    x2Speed = x2Speed * -1;
+  }
+  x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
+
+  // same as above, but for the top and bottom of the canvas
+  if (y2 > width || y2 < 0) {
+    y2Speed = y2Speed * -1;
+  }
+  y2 = y2 + y2Speed;
+
+  circle(x2, y2, d2);
+
 }
